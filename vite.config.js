65a6a2cc9e8ec8ec057d8f01f8f19185a2d121a8
Gonçalo Heleno <goncalo.carvalheiroheleno@heig-vd.ch>
2025-08-29 14:28:02 +0200

    ci: add workflow to publish website

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/website/', // Add the repository name to the path because we do not have a domain name yet.
 })
